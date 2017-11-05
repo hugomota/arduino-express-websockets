a2a02537c167e82bb8a2ed0d5312b593f680e00a
Hugo Mota <hugomota@Hugos-Mac-mini.home>
2017-11-05 17:05:55 +0000

    Broadcast to all/ Add babel

---
+++ index.js
@@ -0,0 +1,23 @@
+import config from './config.json'
+import socketIo from 'socket.io'
+import express from 'express'
+import http from 'http'
+
+const app    = express()
+const server = http.createServer(app)
+const io     = socketIo(server)
+const port   = config.port
+
+server.listen(port);
+console.log('listen ' + port);
+
+io.on('connection', function(socket) {
+  console.log('Client connected...');
+  console.log(Object.keys(io.sockets.sockets));
+
+  socket.on('newArduinoRead', function(data) {
+    console.log('New Data Recieved');
+
+    socket.broadcast.emit('newRead', data);
+  });
+});
