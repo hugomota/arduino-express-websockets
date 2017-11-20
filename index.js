6d8b758c04fb941ea32f2e912a4d3f9b639001d9
Hugo Mota <hmota@rocketfuel.com>
2017-11-20 18:05:07 +0000

    Clean not necessary code for deployment

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import config from './config.json'
 import socketIo from 'socket.io'
 import express from 'express'
 import http from 'http'
@@ -6,20 +5,20 @@ import http from 'http'
 const app    = express()
 const server = http.createServer(app)
 const io     = socketIo(server)
-const port   = config.port
+const port   = 8080
 
 app.get('/', (req, res) => res.send('The Server is Runing!'))
 
 server.listen(port);
 console.log('listen ' + port);
 
-io.on('connection', function(socket) {
-  console.log('Client connected...');
-  console.log(Object.keys(io.sockets.sockets));
+io.on('connection', socket => {
+	console.log('Client connected...');
+ 	console.log(Object.keys(io.sockets.sockets));
 
-  socket.on('newArduinoRead', function(data) {
-    console.log('New Data Recieved');
+	socket.on('newArduinoRead', function(data) {
+		console.log('New Data Recieved');
 
-    socket.broadcast.emit('newRead', data);
-  });
+		socket.broadcast.emit('newRead', data);
+	});
 });
